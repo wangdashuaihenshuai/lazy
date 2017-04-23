67182606fb3d1a5e4fa6ef526613faa22da6a07d
王金海 <jinhai.wang@ele.me>
2017-04-23 23:01:49 +0800

    first commit

---
+++ lazy.js
@@ -0,0 +1,86 @@
+
+const range = function* (from, to) {
+  for(let i = from; i < to; i++) {
+    console.log('range\t', i);
+    yield i;
+  }
+}
+
+const map = function* (flow, solve) {
+  for(const data of flow) {
+    console.log('map\t', data);
+    yield(solve(data));
+  }
+}
+
+const filter = function* (flow, condition) {
+  for(const data of flow) {
+    console.log('filter\t', data);
+    if (condition(data)) {
+      yield data;
+    }
+  }
+}
+
+const stop = function*(flow, condition) {
+  for(const data of flow) {
+    yield data;
+    if (condition(data)) {
+      break;
+    }
+  }
+}
+
+const take = function (flow, number) {
+  let count = 0;
+  const _filter = function (data) {
+    count ++
+    return count >= number;
+  }
+  return stop(flow, _filter);
+}
+
+// for(const n of take(filter(map(range(0, 10000), n => n * 10), n => n % 3 === 0), 2)) {
+//   console.log('num:\t', n, '\n');
+// }
+
+class _Lazy{
+  constructor() {
+    this.iterator = null;
+  }
+
+  range(...args) {
+    this.iterator = range(...args);
+    return this;
+  }
+
+  map(...args) {
+    this.iterator = map(this.iterator, ...args);
+    return this;
+  }
+
+  filter(...args) {
+    this.iterator = filter(this.iterator, ...args);
+    return this;
+  }
+
+  take(...args) {
+    this.iterator = take(this.iterator, ...args);
+    return this;
+  }
+
+  [Symbol.iterator]() {
+    return this.iterator;
+  }
+
+}
+
+function lazy () {
+  return new _Lazy();
+}
+
+const nums = lazy().range(0, 10).map(n => n * 10).filter(n => n % 3 === 0).take(2);
+
+for(let n of nums) {
+  console.log('num:\t', n, '\n');
+}
---
+++ lazy_func.js
@@ -0,0 +1,80 @@
+const over = Symbol();
+
+const isOver = function (_over) {
+  return _over === over;
+}
+
+const range = function (from, to) {
+  let i = from;
+  return function () {
+    if (i < to) {
+      i++
+      console.log('range\t', i);
+      return i
+    }
+    return over;
+  }
+}
+
+const map = function (flow, solve) {
+  return function () {
+    const data = flow();
+    console.log('map\t', data);
+    if (isOver(data)) {
+      return data;
+    }
+
+    return solve(data);
+  }
+}
+
+const filter = function (flow, condition) {
+  return function () {
+    while(true) {
+      const data = flow();
+      if (isOver(data)) {
+        return data;
+      }
+      if(condition(data)) {
+        console.log('filter\t', data);
+        return data;
+      }
+    }
+  }
+}
+
+const stop = function (flow, condition) {
+  let _stop = false;
+  return function () {
+    if (_stop) return over;
+    const data = flow();
+    if (isOver(data)) {
+      return data;
+    }
+    _stop = condition(data);
+    return data;
+  }
+}
+
+const take = function(flow, num) {
+  let i = 0;
+  return stop(flow, (data) => {
+    return ++i >= num;
+  });
+}
+
+const join = function (flow) {
+  const array = [];
+  while(true) {
+    const data = flow();
+    if (isOver(data)) {
+      break;
+    }
+    array.push(data);
+  }
+  return array;
+}
+
+
+const nums = join(take(filter(map(range(0, 20), n => n * 10), n => n % 3 === 0), 2));
+console.log(nums);
